83f353b7e440484a4353c7392d943e23c2608327
zhaochanghui <2636190619@qq.com>
2018-11-05 13:09:56 +0800

    'music-serach'

--- android_views/music/music_list.js
+++ android_views/music/music_list.js
@@ -18,7 +18,9 @@ class MusicList extends Component{
 
     fetchData()
     {
-        fetch(this.state.fetchUrl)
+        let url1 = this.state.fetchUrl+"?kw="+this.state.input_text;
+        alert(url1)
+        fetch(url1)
             .then((response) => response.json())
             .then((responseData) => {
                 // 注意，这里使用了this关键字，为了保证this在调用时仍然指向当前组件，我们需要对其进行“绑定”操作
@@ -36,7 +38,8 @@ class MusicList extends Component{
 
     getInput()
     {
-        alert(this.state.input_text)
+        this.setState({data:[]});
+        this.fetchData();
     }
 
     renderItem({item}){
@@ -86,7 +89,7 @@ class MusicList extends Component{
         return(
             <View style={{flex:1}}>
                 <View style={{flexDirection:'row',height:40}}>
-                    <TextInput  placeholder='yy..'
+                    <TextInput  placeholder=''
                                 editable={true}
                                 onChangeText={this.onchangeInput}
                         style={{flex:1,borderColor:'black',borderWidth: 1}}
--- api/yy.php
+++ api/yy.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Origin: *");
 
 include 'nokogiri.php';
 
- function trimall($str)
+function trimall($str)
 {
     $oldchar = array(" ", "　", "\t", "\n", "\r");
     $newchar = array("", "", "", "", "");
@@ -12,16 +12,43 @@ include 'nokogiri.php';
 }
 
 
-
-$a = isset($_GET['key'])?$_GET['key']:false;
+$a = isset($_GET['key']) ? $_GET['key'] : false;
+$kw = isset($_GET['kw']) ? $_GET['kw'] : false;
 
 $str = file_get_contents('./music.txt');
 
-$newarr = json_decode($str,1);
+$newarr = json_decode($str, 1);
 
-if(!$a) {
+
+//判断是不是搜索
+if ($kw && $kw!='') {
+    $kw = trim($kw);
+
+    $res = [];
     foreach ($newarr as $k => $v) {
-        foreach ($v as $vk=>$vv){
+
+        if (!strpos($v['title'], $kw)) {
+            continue;
+        }
+
+        foreach ($v as $vk => $vv) {
+            $vv = trimall($vv);
+            $newarr[$k][$vk] = $vv;
+        }
+
+        $newarr[$k]['key'] = $k + 1;
+        array_push($res,$newarr[$k]);
+    }
+
+
+
+    $str1 = json_encode($res);
+    echo $str1;
+
+
+} else if (!$a) {   //列表
+    foreach ($newarr as $k => $v) {
+        foreach ($v as $vk => $vv) {
             $vv = trimall($vv);
             $newarr[$k][$vk] = $vv;
         }
@@ -32,16 +59,14 @@ if(!$a) {
 
     $str1 = json_encode($newarr);
     echo $str1;
-}
-
-if($a){
-
-    $index = $a-1;
+} else {
+    //详情
+    $index = $a - 1;
     $one = $newarr[$index];
 
     $pjs = $one['pjs'];
-    $pjs = str_replace('(','',$pjs);
-    $pjs = str_replace(')','',$pjs);
+    $pjs = str_replace('(', '', $pjs);
+    $pjs = str_replace(')', '', $pjs);
     $pjs = trimall($pjs);
     $one['pjs'] = $pjs;
 
